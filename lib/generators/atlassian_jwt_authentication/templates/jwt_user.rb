dc03cbda239eecbbfd18ae31397f17272423824e
Laura Barladeanu <laura.barladeanu@gmail.com>
2016-03-30 16:32:00 +0200

    attr_accessible for jwt_user

--- lib/generators/atlassian_jwt_authentication/templates/jwt_user.rb
+++ lib/generators/atlassian_jwt_authentication/templates/jwt_user.rb
@@ -5,4 +5,6 @@ class JwtUser < ActiveRecord::Base
   databases = YAML::load(IO.read('config/database_<%= database_name %>.yml'))
   establish_connection databases[Rails.env]
   <% end %>
+
+  attr_accessible :jwt_token_id, :user_key
 end
