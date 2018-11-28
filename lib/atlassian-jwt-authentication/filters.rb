cf6815b5185cbf11a9382ff1b66aa471801b8cef
Pawel Niewiadomski <11110000b@gmail.com>
2018-11-28 17:46:31 +0100

    Using module attribute to control jwt token expiration verification

--- lib/atlassian-jwt-authentication/filters.rb
+++ lib/atlassian-jwt-authentication/filters.rb
@@ -143,10 +143,7 @@ module AtlassianJwtAuthentication
         return false
       end
 
-      # Decode the JWT parameter without verification
-      verify_expiration = ENV.fetch('JWT_VERIFY_EXPIRATION', 'true') != 'false'
-
-      decoded = JWT.decode(jwt, nil, false, {verify_expiration: verify_expiration})
+      decoded = JWT.decode(jwt, nil, false, {verify_expiration: verify_jwt_expiration})
 
       # Extract the data
       data = decoded[0]
@@ -173,7 +170,7 @@ module AtlassianJwtAuthentication
       # The JWT gem has changed the way you can access the decoded segments in v 1.5.5, we just handle both.
       if JWT.const_defined?(:Decode)
         options = {
-            verify_expiration: verify_expiration,
+            verify_expiration: verify_jwt_expiration,
             verify_not_before: true,
             verify_iss: false,
             verify_iat: false,
