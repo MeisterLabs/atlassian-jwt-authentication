42ad0f3b936ef515bb53aa40998ddabf522ca95c
Pawel Niewiadomski <11110000b@gmail.com>
2019-01-14 17:02:11 +0100

    Don't set attributes in initialize as we update them bellow

--- lib/atlassian-jwt-authentication/filters.rb
+++ lib/atlassian-jwt-authentication/filters.rb
@@ -55,10 +55,7 @@ module AtlassianJwtAuthentication
           if user[:username].present? && user[:display_name].present? &&
               user[:uuid].present? && user[:type].present? && user[:type] == 'user'
 
-            jwt_user = current_jwt_auth.jwt_users.find_or_initialize_by(user_key: user[:uuid]) do |jwt_user|
-              jwt_user.name = user[:username]
-              jwt_user.display_name = user[:display_name]
-            end
+            jwt_user = current_jwt_auth.jwt_users.find_or_initialize_by(user_key: user[:uuid])
 
             jwt_user.update!(
               name: user[:username],
