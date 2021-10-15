92f2f75a712bdf9b8e91b76beab5b353e6dc23db
Pawel Niewiadomski <pawel@herocoders.com>
2021-10-15 16:56:07 +0200

    Use commit as build number

--- lib/atlassian-jwt-authentication/version.rb
+++ lib/atlassian-jwt-authentication/version.rb
@@ -1,3 +1,15 @@
 module AtlassianJwtAuthentication
-  VERSION = '0.8.0'.freeze
+  MAJOR_VERSION = "0"
+  MINOR_VERSION = "8"
+  PATH_VERSION = "0"
+  BUILD_NUMBER = ENV["GITHUB_SHA"] && "+#{ENV["GITHUB_SHA"][0..6]}"
+
+  VERSION =
+    (
+      [
+        MAJOR_VERSION,
+        MINOR_VERSION,
+        PATH_VERSION
+      ].join(".") + BUILD_NUMBER
+    ).freeze
 end
